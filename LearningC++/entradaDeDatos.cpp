65d0d6453260cee173815783dce03cb083fc94b4
Magdaleno0100 <Magdaleno0100@gmail.com>
2020-09-13 18:16:53 -0500

    Se agrega ejemplo de entrada de datos

---
+++ LearningC++/entradaDeDatos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a = 0, b = 0, c = 0, d =0;
+	
+	cin >> a >>b >> c >>d;
+	
+	
+	cout << "Value of a " << a << endl;
+	cout << "Value of b " << b << endl;
+	cout << "Value of c " << c << endl;
+	cout << "Value of d " << d << endl;
+	
+	return 0;
+}//end main
